f3d0ee5d2ded6f684c9e8ab98437762f53ee704d
seumbae <ohcm0013@gmail.com>
2021-07-09 19:47:59 +0900

    No.10430

--- level1/10430.cpp
+++ level1/10430.cpp
@@ -8,7 +8,7 @@ int main(){
     cin >> a >> b >> c;
 
     cout << (a+b)%c << endl;
-    cout << ((a%c) + (b%c)%c) << endl;
+    cout << ((a%c) + (b%c))%c << endl;
     cout << (a*b)%c << endl;
     cout << ((a%c) * (b%c))%c << endl;
 }
