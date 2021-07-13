0b2517276e14fb9f2069f3628861a3ee66dfca13
seumbae <ohcm0013@gmail.com>
2021-07-13 19:15:00 +0900

    No.10871

---
+++ level3/10871.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n, x, a;
+
+    cin >> n >> x;
+
+    for(int i=0;i<n;i++){
+        cin >> a;
+        if(a < x)
+            cout << a << " ";
+    }
+    
+}
