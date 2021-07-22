a074a6d790470cf7e105b5807d592978f7d606da
seumbae <ohcm0013@gmail.com>
2021-07-22 17:39:11 +0900

    No.15596

--- level6/15596.cpp
+++ level6/15596.cpp
@@ -1,28 +1,12 @@
-#include<iostream>
 #include<vector>
 
 using namespace std;
 
-long sum(vector<int> &a){
-    long ans=0;
+long long sum(vector<int> &a){
+    long long ans=0;
 
     for(int i=0;i<a.size();i++){
         ans +=a[i];    
     }
     return ans;
-}
-
-int main(){
-    int n,input;
-    vector<int> v;
-
-    cin >> n;
-
-    for(int i=0;i<n;i++){
-        cin >> input;
-        v.push_back(input);
-    }
-    
-    long ans = sum(v);
-    cout << ans << endl;
 }
