ec6a2f6aea72acd1c1356eebc7870ac5f0a501fd
seumbae <ohcm0013@gmail.com>
2021-08-13 20:02:37 +0900

    need to be modified

--- level9/1929.cpp
+++ level9/1929.cpp
@@ -3,25 +3,14 @@
 
 using namespace std;
 
+
 void solve(int m, int n){
-    vector<int> v;
+    int *arr = new int[n+1];
 
-    for(int i=m; i<=n; i++)
-        v.push_back(i);
+    for(int i=0; i<n+1; i++)
+        *(arr+i) = i;
     
-    for(int i=2; i<n; i++){
-        for(int j=0; j<v.size(); j++){
-            if(v[j] == 0 || v[j] == i)
-                continue;
-            if((v[j]%i) == 0)
-                v[j] = 0;
-        }
-    }
-
-    for(auto a:v){
-        if(a)   cout << a << endl;
-    }
-    
+        
 }
 
 int main(){
