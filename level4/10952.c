d368918def3626c081af29c08fa33f205f494c25
seumbae <ohcm0013@gmail.com>
2021-10-27 19:26:40 +0900

    No.10952

--- level4/10952.c
+++ level4/10952.c
@@ -2,12 +2,12 @@
 #include<stdbool.h>
 
 int main(){
-    int a,b;
+    int a, b;
 
     while(true){
-        scanf("%d%d",&a,&b);
-        if(a==0 && b==0 )
+        scanf("%d %d",&a,&b);
+        if(a==0 && b==0)
             break;
         printf("%d\n",a+b);
-    }    
+    }
 }
