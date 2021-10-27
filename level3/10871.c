476f1047262b0e1e8c22921998c74b9d51fca3cb
seumbae <ohcm0013@gmail.com>
2021-10-27 17:29:13 +0900

    No.10871

---
+++ level3/10871.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int n, x, num;
+    scanf("%d %d",&n,&x);
+    
+    for(int i=0; i<n; i++){
+        scanf("%d",&num);
+        if(x > num)
+            printf("%d ", num);
+    }
+}
