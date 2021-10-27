c9ab3a637302b3c88b2c726050f98b1d4024f45d
seumbae <ohcm0013@gmail.com>
2021-10-27 17:05:45 +0900

    No.2739

--- level3/2739.c
+++ level3/2739.c
@@ -2,10 +2,8 @@
 
 int main(){
     int n;
-
     scanf("%d",&n);
 
-    for(int i=1;i<=9;i++){
-        printf("%d * %d = %d\n",n,i,n*i);
-    }
+    for(int i=0;i<9;i++)
+        printf("%d * %d = %d\n",n, i+1, n * (i+1));
 }
