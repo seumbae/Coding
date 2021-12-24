1359d2ede98f083709e928159b3b445e96753093
seumbae <ohcm0013@gmail.com>
2021-12-24 15:50:55 +0900

    No.2751 QuickSort TimeOver

---
+++ level12/2751.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+//QuickSort Avg: O(N*logN) but, Worst: O(N^2) 
+void quickSort(int* arr, int start, int end){
+    if(start >= end) return; // the number of data is 1
+    int pivot = start;
+    int left = pivot + 1;
+    int right = end;
+
+    while(left <= right){
+        //pivot보다 큰 수 찾기
+        while(left <= end && arr[pivot] >= arr[left]){
+            left++;
+        }
+        //pivot보다 작은 수 찾기
+        while(right > start && arr[pivot] <= arr[right]){
+            right--;
+        }
+        if(left > right) // left의 인덱스가 right보다 클 때
+            swap(arr[pivot], arr[right]);
+        else
+            swap(arr[left], arr[right]);
+    }
+
+    //각각 왼쪽, 오른쪽, 현재 right는 중앙에 있는 pivot을 가리키고 있음
+    quickSort(arr, start, right-1);
+    quickSort(arr, right+1, end);
+}
+
+//MergeSort only O(N*logN)
+void mergeSort(){
+
+}
+
+//HeapSort
+void heapSort(){
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int* arr = new int[n];
+    for(int i=0; i<n; i++)
+        cin >> *(arr+i);
+    quickSort(arr, 0, n-1);
+
+    for(int i=0; i<n; i++)
+        cout << *(arr+i) << endl;
+}
