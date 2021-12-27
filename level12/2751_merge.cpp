4831d5a8f95c163e46dfdb84e4d9eb9ca26c88a3
seumbae <ohcm0013@gmail.com>
2021-12-27 13:09:07 +0900

    No.2751

--- level12/2751_merge.cpp
+++ level12/2751_merge.cpp
@@ -1,36 +1,10 @@
 #include<iostream>
-#include<algorithm>
 
 using namespace std;
 
 int* sorted;
 
-//QuickSort Avg: O(N*logN) but, Worst: O(N^2) 
-void quickSort(int* arr, int start, int end){
-    if(start >= end) return; // the number of data is 1    
-    int pivot = start;
-    int left = start + 1;
-    int right = end;
-
-    while(left <= right){
-        //pivot보다 큰 수 찾기
-        while(left <= end && arr[pivot] >= arr[left]){
-            left++;
-        }
-        //pivot보다 작은 수 찾기
-        while(right > start && arr[pivot] <= arr[right]){
-            right--;
-        }
-        if(left > right) // left의 인덱스가 right보다 클 때
-            swap(arr[pivot], arr[right]);
-        else
-            swap(arr[left], arr[right]);
-    }
-
-    //각각 왼쪽, 오른쪽, 현재 right는 중앙에 있는 pivot을 가리키고 있음
-    quickSort(arr, start, right-1);
-    quickSort(arr, right+1, end);
-}
+//MergeSort O(N*logN)
 void merge(int* arr, int start, int middle, int end){
     int i = start;
     int j = middle + 1;
@@ -81,9 +55,8 @@ int main(){
 
     for(int i=0; i<n; i++)
         cin >> *(arr+i);
-    //quickSort(arr, 0, n-1);
-    //mergeSort(arr, 0, n-1);
-    sort(arr, arr+n);
+    
+    mergeSort(arr, 0, n-1);
     
     for(int i=0; i<n; i++)
         cout << *(arr+i) << "\n";
