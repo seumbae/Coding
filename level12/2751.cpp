0e0080ef93c821954cfd7682ae56dcd5ee89aa6f
seumbae <ohcm0013@gmail.com>
2021-12-27 13:01:57 +0900

    No.2751 add mergeSort

--- level12/2751.cpp
+++ level12/2751.cpp
@@ -2,11 +2,13 @@
 
 using namespace std;
 
+int* sorted;
+
 //QuickSort Avg: O(N*logN) but, Worst: O(N^2) 
 void quickSort(int* arr, int start, int end){
-    if(start >= end) return; // the number of data is 1
+    if(start >= end) return; // the number of data is 1    
     int pivot = start;
-    int left = pivot + 1;
+    int left = start + 1;
     int right = end;
 
     while(left <= right){
@@ -28,25 +30,59 @@ void quickSort(int* arr, int start, int end){
     quickSort(arr, start, right-1);
     quickSort(arr, right+1, end);
 }
+void merge(int* arr, int start, int middle, int end){
+    int i = start;
+    int j = middle + 1;
+    int k = start;
 
-//MergeSort only O(N*logN)
-void mergeSort(){
-
+    //작은 순서대로 배열에 삽입
+    while(i<= middle && j<=end){
+        if(arr[i] <= arr[j]){
+            sorted[k] = arr[i];
+            i++;
+        }else{
+            sorted[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+    //남은 데이터 삽입
+    if(i > middle){
+        for(;j<=end;j++){
+            sorted[k] = arr[j];
+            k++;
+        }
+    }else{
+        for(;i<=middle; i++){
+            sorted[k] = arr[i];
+            k++;
+        }
+    }
+    for(int t=start; t<=end; t++){
+        arr[t] = sorted[t];
+    }
 }
-
-//HeapSort
-void heapSort(){
-
+void mergeSort(int* arr, int start, int end){
+    //크기가 1보다 큰 경우
+    if(start < end){
+        int middle = (start + end)/2;
+        mergeSort(arr, start, middle);
+        mergeSort(arr, middle+1, end);
+        merge(arr, start, middle, end);
+    }
 }
 
 int main(){
     int n;
     cin >> n;
     int* arr = new int[n];
-    for(int i=0; i<n; i++)
-        cin >> *(arr+i);
-    quickSort(arr, 0, n-1);
+    sorted = new int[n];
 
     for(int i=0; i<n; i++)
-        cout << *(arr+i) << endl;
+        cin >> *(arr+i);
+    //quickSort(arr, 0, n-1);
+
+    mergeSort(arr, 0, n-1);
+    for(int i=0; i<n; i++)
+        cout << *(arr+i) << "\n";
 }
