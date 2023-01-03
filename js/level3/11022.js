d85d100c6872c0675fb66fa758f544d431812cca
seumbae <ohcm0013@gmail.com>
2023-01-03 17:20:27 +0900

    No.11022 A+B - 8

---
+++ js/level3/11022.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+const [n, ...arr] = fs.readFileSync(filePath).toString().trim().split('\n');
+
+arr.forEach((item, idx) => {
+  const [a, b] = item.split(' ');
+  console.log(`Case #${idx+1}: ${a} + ${b} = ${Number(a) + Number(b)}`)
+})
