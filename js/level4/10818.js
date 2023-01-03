2672cc65e0bdfe58cb149e0e6d7b89e44f2c8b1c
seumbae <ohcm0013@gmail.com>
2023-01-03 19:22:24 +0900

    No.10818 최소, 최대

---
+++ js/level4/10818.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const list = input[1].split(' ').map(Number);
+
+console.log(Math.min(...list), Math.max(...list));
+
