09cd862724deaf42d1861febd7322d5871f78c82
seumbae <ohcm0013@gmail.com>
2023-01-17 19:24:46 +0900

    No.2750 수 정렬하기 (O(n²), 버블정렬, 선택정렬)

--- js/level10/2750.js
+++ js/level10/2750.js
@@ -6,7 +6,7 @@ const [n, ...input] = fs.readFileSync(filePath).toString().trim().split("\n").ma
  * O(n^2): 삽입정렬, 거품정렬, 선택정렬
  */
 
-// 거품정렬 Best(n^2), Avg(n^2)
+/* 거품정렬 Best(n^2), Avg(n^2), Worst(n^2)
 const bubble = (elem) => {
 	for (i = 0; i < n ; i++) {
 		for (j = 0 ; j < n - i; j++) {
@@ -19,5 +19,19 @@ const bubble = (elem) => {
 };
 
 bubble(input);
+*/
 
-//Insertion
+//Selection Best(n^2), Avg(n^2), Worst(n^2)
+const Selection = (elem) => {
+  for(let i=0; i<n; i++){
+    let min_idx = i;
+    for(let j=i+1; j<n; j++){
+      if(elem[min_idx] > elem[j]){
+        min_idx = j;
+      }
+    }
+    [elem[i], elem[min_idx]] = [elem[min_idx], elem[i]];
+  }
+  console.log(elem.join('\n'));
+}
+Selection(input);
