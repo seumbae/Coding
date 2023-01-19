da1fa18bd5075a2f18a41df7db37efc18f82985a
seumbae <ohcm0013@gmail.com>
2023-01-19 23:26:10 +0900

    No.2750 수 정렬하기 (O(n²), 버블정렬, 선택정렬, 삽입정렬)

--- js/level10/2750.js
+++ js/level10/2750.js
@@ -6,7 +6,7 @@ const [n, ...input] = fs.readFileSync(filePath).toString().trim().split("\n").ma
  * O(n^2): 삽입정렬, 거품정렬, 선택정렬
  */
 
-/* 거품정렬 Best(n^2), Avg(n^2), Worst(n^2)
+/* 거품정렬 Best(n^2), Avg(n^2), Worst(n^2) 계속해서 양옆과 비교
 const bubble = (elem) => {
 	for (i = 0; i < n ; i++) {
 		for (j = 0 ; j < n - i; j++) {
@@ -21,7 +21,7 @@ const bubble = (elem) => {
 bubble(input);
 */
 
-//Selection Best(n^2), Avg(n^2), Worst(n^2)
+/* Selection Best(n^2), Avg(n^2), Worst(n^2) 최소값의 인덱스 찾기
 const Selection = (elem) => {
   for(let i=0; i<n; i++){
     let min_idx = i;
@@ -35,3 +35,17 @@ const Selection = (elem) => {
   console.log(elem.join('\n'));
 }
 Selection(input);
+*/
+
+// Insertion Best(n), Avg(n^2), Worst(n^2)
+const Insertion = (elem) => {
+  for(let i=1; i<n; i++){
+    let j=i;
+    while((elem[j] < elem[j-1]) && j>0){
+      [elem[j], elem[j-1]] = [elem[j-1], elem[j]];
+      j--;
+    }
+  }
+  console.log(elem.join('\n'));
+}
+Insertion(input);
