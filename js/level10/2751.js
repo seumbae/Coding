7814f9f702c052f0f6699f0577618abedc14a177
seumbae <ohcm0013@gmail.com>
2023-01-25 13:36:41 +0900

    No.2751 수 정렬하기 (O(nlogn, Tim)

--- js/level10/2751.js
+++ js/level10/2751.js
@@ -1,10 +1,19 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
-const [input, score] = fs.readFileSync(filePath).toString().trim().split("\n");
+const [input, ...numbers] = fs.readFileSync(filePath).toString().trim().split("\n");
 
 /**
  * O(nlogn) 정렬 알고리즘
+ * Tim - Best(n), Avg(nlogn), Worst(nlogn);
  * Quick - Best(nlogn), Avg(nlogn), Worst(n^2) 
  * Merge - Best(nlogn), Avg(nlogn), Worst(nlogn)
  * Heap - Best(nlogn), Avg(nlogn), Worst(nlogn)
  */
+
+/**
+ * Tim Sort
+ */
+
+const ans = numbers.map(Number).sort((a,b) => a-b);
+console.log(ans.join('\n'));
+
