fb5b35284ce23de2394e5fbee55a18edabc36dc7
seumbae <ohcm0013@gmail.com>
2023-01-25 18:15:15 +0900

    No.11651 좌표 정렬하기 2

---
+++ js/level10/11651.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
+const [N, ...input] = fs.readFileSync(filePath).toString().trim().split('\n');
+
+ans = input.map((elem) => elem.split(' ').map(Number)).sort((a, b) => {
+  if(a[1] === b[1])
+    return a[0] - b[0];
+  return a[1] - b[1];
+}).join('\n').replace(/,/g, " ");
+
+console.log(ans);
