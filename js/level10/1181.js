bebe263dcc38ab22bdd2d93127a5dff58ed6551f
seumbae <ohcm0013@gmail.com>
2023-01-26 21:47:05 +0900

    No.1181 단어 정렬 (Using Set)

--- js/level10/1181.js
+++ js/level10/1181.js
@@ -2,21 +2,19 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 const [N, ...input] = fs.readFileSync(filePath).toString().trim().split('\n');
 
+/**
+ * Set 자료구조. 중복을 허락하지 않음
+ */
+const set = [...new Set(input)];
+
 const compare = (a, b) => {
   if(a.length === b.length){
-    if(a > b) return 1;
-    if(a < b) return -1;
+    return a < b ? -1 : 1;
   }
   else 
     return a.length - b.length;
 }
 
-const sorted = input.sort((a,b) => compare(a,b)) 
-let ans = '';
-for(let i=0; i < sorted.length; i++){
-  if(sorted[i] === sorted[i+1])
-    continue;
-  ans += `${sorted[i]}\n`
-}
-console.log(ans);
+const sorted = set.sort((a,b) => compare(a,b)).join('\n');
+console.log(sorted);
 
