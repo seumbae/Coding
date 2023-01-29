8050e5de87fdd64e9a2e456e9ee33c9a80985d89
seumbae <ohcm0013@gmail.com>
2023-01-29 23:49:14 +0900

    No.18870 좌표 압축 (Object, Binary Search)

--- js/level10/18870.js
+++ js/level10/18870.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 let [N, input] = fs.readFileSync(filePath).toString().trim().split('\n');
-
+/*
+  Object의 key, value 사용
 input = input.split(' ').map(Number);
 const set = new Set(input);
 
@@ -16,3 +17,33 @@ for(const elem of input){
 }
 
 console.log(ans);
+*/
+
+/**
+ * lowerbound func 구현, 이진 탐색의 경우 정렬이 되어 있어야함.
+ */
+
+const lowerbound = (arr, elem) => {
+  const len = arr.length;
+  let [low, high] = [0, len-1]
+
+  while(low <= high){
+    const mid = Math.floor((low + high) / 2);
+    if(arr[mid] === elem){
+      return mid;
+    }
+    if(arr[mid] < elem){
+      low = mid + 1;
+    }
+    else{
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
+
+input = input.split(' ').map(Number);
+const uniq = [...new Set(input)].sort((a, b) => a-b);
+
+console.log(input.map((elem) => lowerbound(uniq, elem)).join(' '));
+
