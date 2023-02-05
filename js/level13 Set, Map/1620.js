bca4c79ac9c067148af8d3fff96b2cffbebc58aa
seumbae <ohcm0013@gmail.com>
2023-02-05 16:39:55 +0900

    No.1620 나는야 포켓몬 마스터 이다솜

--- js/level13 Set, Map/1620.js
+++ js/level13 Set, Map/1620.js
@@ -3,18 +3,15 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 let [testCase, ...input] = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const [N, M] = testCase.split(' ').map(e=>+e);
-const map = new Map(input.slice(0,N).map((val, idx) => [idx, val]));
+const poketmon = input.slice(0, N);
+const map = new Map(poketmon.map((val, idx) => [val, idx]));
 const problem = input.slice(N);
 let ans = '';
 
 problem.map(elem => {
   if(isNaN(parseInt(elem))) {
-    for(let [key, val] of map){ //TODO: 이 부분이 문제인듯함
-      if(val === elem){
-        ans += `${key+1}\n`
-      }
-    }
+    ans += `${map.get(elem)+1}\n`;
   }
-  else ans += `${map.get(+elem-1)}\n`;
+  else ans += `${poketmon[+elem-1]}\n`
 })
 console.log(ans);
