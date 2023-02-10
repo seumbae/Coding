3d2b6cca0edf3e5c7c242006187810bcc05c1af4
seumbae <ohcm0013@gmail.com>
2023-02-10 20:20:55 +0900

    No. 1715 TODO: heap

--- js/level19 Greedy/1715.js
+++ js/level19 Greedy/1715.js
@@ -1,18 +1,19 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 let [N, ...input] = require("fs").readFileSync(filePath).toString().trim().split('\n').map(Number);
-
-// TODO: 아래와 같은 논리를 사용
-// 단순히 정렬 후 더해나가면 안된다.
-// 이를테면, 5, 6, 7, 8의 경우 먼저 5, 6을 더하고 7,8을 더하는 순으로 나아가야한다. 즉 가자으 적은 두 뭉치를 더해나가야 한다.
 // https://www.acmicpc.net/board/view/90899
-// 
-input.sort((a,b) => a-b);
-
-let ans = 0;
-
-for(let i=0; i<N-1; i++){
-  input[i+1] += input[i];
-  ans += input[i+1];
+// 값에 대한 잦은 수정이 있는경우, 계속 정렬을 해야하는 경우는 heap을 사용하는게 효과적입니다.
+if(N === 1){
+  console.log(0);
+  return 0;
 }
 
-console.log(ans)
+let ans = 0
+
+while(input.length !== 1){
+  input.sort((a,b) => a-b); // TODO: 이 부분에서 계속해서 정렬을 해주기 때문에 메모리 초과가 난다고 한다.
+  ans += input[0] + input[1]
+  input.push(input[0] + input[1]);
+  input.shift();
+  input.shift();
+}
+console.log(ans);
