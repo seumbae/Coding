e47a39c78acea6274564f58d69029da5b6f51aae
seumbae <ohcm0013@gmail.com>
2023-02-11 23:51:08 +0900

    No. 1715 TODO: heap

--- js/level19 Greedy/1715.js
+++ js/level19 Greedy/1715.js
@@ -2,18 +2,15 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 let [N, ...input] = require("fs").readFileSync(filePath).toString().trim().split('\n').map(Number);
 // https://www.acmicpc.net/board/view/90899
 // 값에 대한 잦은 수정이 있는경우, 계속 정렬을 해야하는 경우는 heap을 사용하는게 효과적입니다.
+
+const Heap = () => {
+  
+
+}
 if(N === 1){
   console.log(0);
   return 0;
 }
 
-let ans = 0
-
-while(input.length !== 1){
-  input.sort((a,b) => a-b); // TODO: 이 부분에서 계속해서 정렬을 해주기 때문에 메모리 초과가 난다고 한다.
-  ans += input[0] + input[1]
-  input.push(input[0] + input[1]);
-  input.shift();
-  input.shift();
-}
-console.log(ans);
+//처음에는 sort 메소드를 통해 정렬 이후 heap 자료구조를 이용하면 정렬을 하지 않아도 된다. 
+input.sort((a,b) => a-b);
