7d7de270d33c7c334ca16863db0ac26b5baaafac
seumbae <ohcm0013@gmail.com>
2023-02-14 00:28:49 +0900

    No.1339 단어 수학

--- js/level19 Greedy/1339.js
+++ js/level19 Greedy/1339.js
@@ -1,24 +1,29 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
-let [N, ...input] = require("fs").readFileSync(filePath).toString().trim().split('\n');
+let [N, ...input] = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
 // https://www.acmicpc.net/board/view/85400
+let weight = {};
 
-input = input.map((elem) => elem.split('').reverse().join('')).sort((a,b) => b.length - a.length);
-let obj = {};
+// weights
+input.forEach((elem) => {
+	for (let i = 0; i < elem.length; i++) {
+    let pow = 10 ** (elem.length-i-1);
+		if (weight[elem[i]]) {
+			weight[elem[i]] += pow;
+		} else {
+			weight[elem[i]] = pow;
+		}
+	}
+});
+
+let num = 9;
+weight = new Map(Object.entries(weight).sort((a,b) => b[1] - a[1]).map((elem) => [elem[0],num--]));
 
 let ans = 0;
-let num = 9; // 가장 자릿수가 많은 문자열의 첫 문자를 무조건 9로 주면 안된다.
-let len = input[0].length
-for(let i=len-1; i>=0; i--){
-  input.forEach((elem) =>{
-    let pow = 10 ** i;
-    if(obj[elem[i]] === undefined && elem[i] !== undefined){
-      obj[elem[i]] = num--;
-      ans += parseInt(obj[elem[i]] * pow);
-    }
-    else if(elem[i] !== undefined && obj[elem[i]] !== undefined){
-      ans += parseInt(obj[elem[i]] * pow);
-    }
-  })
-}
+input.forEach((elem) => {
+	for (let i = 0; i < elem.length; i++) {
+		let pow = 10 ** (elem.length - i - 1);
+		ans += parseInt(weight.get(elem[i])) * pow;
+	}
+});
 console.log(ans);
