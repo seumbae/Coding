fbced2a56f6c35eb5aa6a861fac8b30b85b0b375
seumbae <ohcm0013@gmail.com>
2023-02-15 23:24:49 +0900

    No.1202 보석 도둑 (시간초과)

--- js/level19 Greedy/1202.js
+++ js/level19 Greedy/1202.js
@@ -1,25 +1,77 @@
+class Heap {
+  constructor(){
+    this.heap = []
+  }
+  size() {
+    return this.heap.length;
+  }
+
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+
+  push(price) {
+    this.heap.push(price);
+
+    let idx = this.heap.length -1;
+    let parent = Math.floor((idx-1)/2);
+
+    while(0 <= parent && this.heap[parent] < this.heap[idx]){
+      this.swap(parent, idx);
+
+      idx = parent;
+      parent = Math.floor((idx-1)/2);
+    }
+  }
+
+  pop() {
+    if(this.heap.length === 1)
+      return this.heap.pop();
+
+    const item = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    
+    let parent = 0;
+    
+    while(parent < this.size()){
+      let left = parent*2 + 1;
+      let right = parent*2 + 2;
+      if(this.size()-1 < left) break;
+
+      let idx = parent;
+
+      if(this.heap[left] && this.heap[right]){
+        idx = this.heap[left] < this.heap[right] ? right : left;
+      }
+
+      if(this.heap[parent] > this.heap[idx]) break;
+
+      this.swap(parent, idx);
+      parent = idx;
+    }
+    return item;
+  }
+}
+
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
 const [N, K] = input.shift().split(" ").map(Number);
 
-const Jewelry = input.slice(0, N).map((elem) => elem.split(" ").map(Number)).sort((a,b) => {
-  if(a[1] === b[1]) return a[0]-b[0];
-  return b[1]-a[1];
-});
+const Jewels = input.slice(0, N).map((elem) => elem.split(" ").map(Number)).sort((a,b) => {a[0]-b[0];});
 const Bags = input.slice(N).map(Number).sort((a,b) => a-b);
 
+const heap = new Heap();
 let ans = 0;
+
+let idx = 0;
 Bags.forEach((elem) => {
-  for(let i=0; i<Jewelry.length;){ // 단순히 for문으로는 시간초과 남. nlog(n)의 사간복잡도로 구현해야함.
-    if(Jewelry[i][0] <= elem){
-      ans += Jewelry[i][1];
-      Jewelry.shift();
-      break;
-    }
-    else{
-      Jewelry.push(Jewelry.shift());
-    }
+  while(idx<N && Jewels[idx][0] <= elem){
+    heap.push(Jewels[idx++][1]);
+  }
+  if(heap.size()){
+    ans += heap.pop();
   }
 })
-console.log(ans);
+
+console.log(ans)
