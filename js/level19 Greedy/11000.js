29a3a5e07bdf54542aafbc405a3aeac93160d394
seumbae <ohcm0013@gmail.com>
2023-02-16 22:52:34 +0900

    No.11000 강의실 배정 (TODO)

--- js/level19 Greedy/11000.js
+++ js/level19 Greedy/11000.js
@@ -1,18 +1,20 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 const N = +input.shift();
-const room = input.map((e) => e.split(' ').map(v=>+v)).sort((a,b) => a[0] - b[0]);
+let room = input.map((e) => e.split(' ').map(v=>+v)).sort((a,b) => a[0] - b[0]);
+// TODO: 우선순위 큐 
+
 
 let ans = 0;
-// console.log(room)
 while(room.length){
   let target = room.shift();
   for(let i=0; i<room.length; i++){
     if(target[1] <= room[i][0]){
       target = room[i];
-      room.splice(i--, 1); //이거 때문에 시간초과가 뜨는거 같음
+      room.splice(i--, 1); //이거 때문에 시간초과가 뜨는거 같음X 애초에 큰 값이 들어오면 시간초과가 날 수밖에 없음 반복문을 줄여야함
     }
   }
   ans++;
 }
+
 console.log(ans);
