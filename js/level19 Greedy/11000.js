d5582f3dd3e02cacee69632051f5b4cbfd52d57a
seumbae <ohcm0013@gmail.com>
2023-02-17 16:54:33 +0900

    No.11000 강의실 배정 (Failed)

--- js/level19 Greedy/11000.js
+++ js/level19 Greedy/11000.js
@@ -1,27 +1,77 @@
-const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
-const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
-const N = +input.shift();
-let room = input.map((e) => e.split(' ').map(v=>+v)).sort((a,b) => a[0] - b[0]);
-// TODO: Min Heap
-// https://jun-choi-4928.medium.com/javascript%EB%A1%9C-heap-priority-queue-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-8bc13bf095d9
-console.log(room);
-let ans = 0;
-
 class MinHeap {
   constructor() {
     this.store = [];
   }
 
   push = (elem) => {
+    this.store.push(elem);
 
+    let elemIdx = this.store[this.size() - 1];
+    let parentIdx = Math.floor((elemIdx-1)/2);
+
+    while(this.store[elemIdx] < this.store[parentIdx]){
+      this.swap(elemIdx, parentIdx);
+
+      elemIdx = parentIdx;
+      parentIdx = Math.floor((elem-1)/2);
+    }
   }
 
   pop = () => {
+    if(this.size() === 1) return this.store.pop();
 
+    const ret = this.store[0];
+    this.store[0] = this.store.pop();
+
+    let parent = 0 ;
+    while(parent < this.size()){
+      let left = parent * 2 + 1;
+      let right = parent * 2 + 2;
+      if(this.size() <= left) break;
+
+      let idx = parent;
+      if(this.store[left] < this.store[idx]) idx = left;
+      if(this.store[right] < this.store[idx]) idx = right;
+
+      if(idx === parent) break;
+
+      this.swap(parent, idx);
+      parent = idx;
+    }
+
+    return ret;
   }
 
   size = () => {
     return this.store.length;
   }
+
+  swap = (a, b) => {
+    [this.store[a], this.store[b]] = [this.store[b], this.store[a]];
+  }
+
+  top = () => {
+    return this.store[0];
+  }
 }
-console.log(ans);
+// TODO: Min Heap
+// https://jun-choi-4928.medium.com/javascript%EB%A1%9C-heap-priority-queue-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-8bc13bf095d9
+const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const N = +input.shift();
+let room = input.map((e) => e.split(' ').map(v=>+v)).sort((a,b) => a[0] - b[0]);
+
+const heap = new MinHeap();
+
+heap.push(room[0][1]);
+for(let i=1; i<N; i++){
+  if(room[i][0] <= heap.top()){
+    heap.push(room[i][1]);
+  }
+  else{
+    heap.pop();
+    heap.push(room[i][1]);
+  }
+}
+console.log(heap)
+console.log(heap.size());
