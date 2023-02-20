b0a34a9e248233ea5cea32ba712a4365f83f460b
seumbae <ohcm0013@gmail.com>
2023-02-20 17:50:18 +0900

    No.1260 DFS와 BFS

--- js/BFS:DFS/1260.js
+++ js/BFS:DFS/1260.js
@@ -1,14 +1,33 @@
+const dfs = (v) => {
+  dfsVisited[v] = true;
+  
+  dfsAns.push(v);
+  graph[v].forEach((node) => {
+    if(!dfsVisited[node]){
+      dfs(node);
+    }
+  })
+}
+
+const bfs = (v) => {
+  const que = [v];
+  bfsVisited[v] = true;
+
+  while(que.length){
+    const node = que.shift();
+    bfsAns.push(node);
+    graph[node].forEach((elem) => {
+      if(!bfsVisited[elem]){
+        que.push(elem);
+        bfsVisited[elem] = true;
+      }
+    })
+  }
+}
+
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 let [T, ...input] = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-const dfs = (graph, node, visited) => {
-  //TODO:
-}
-
-const bfs = (graph, node, visited) => {
-  //TODO:
-}
-console.log(input)
 const [N,M,V] = T.split(' ').map(e=>+e);
 const graph = [...new Array(N+1)].map(() => []);
 input.forEach((elem) => {
@@ -16,8 +35,11 @@ input.forEach((elem) => {
   graph[from].push(to);
   graph[to].push(from);
 })
-
 let dfsVisited = new Array(N).fill(false);
+let dfsAns = [];
 let bfsVisited = new Array(N).fill(false);
-
-//DFS, BFS 출력
+let bfsAns = [];
+graph.forEach((elem) => elem.sort((a, b) => a-b));
+dfs(V);
+bfs(V);
+console.log(`${dfsAns.join(' ')}\n${bfsAns.join(' ')}`);
