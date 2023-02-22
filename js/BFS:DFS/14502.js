5dd1dab81fa40f0d46d157103f16fa9c4df5263f
seumbae <ohcm0013@gmail.com>
2023-02-22 22:50:14 +0900

    No.14502 연구소

--- js/BFS:DFS/14502.js
+++ js/BFS:DFS/14502.js
@@ -3,68 +3,43 @@ let input = require("fs").readFileSync(filePath).toString().trim().split('\n');
 
 const [N, M] = input.shift().split(' ').map(e=>+e);
 let graph = input.map((elem) => elem.split(' ').map(e=>+e));
-let copyGraph = [...new Array(N)].map(() => []);
-const virus = []
-graph.map((elem, idx1) => elem.map((e, idx2) => {
-  if(e === 2) virus.push([idx1, idx2]);
+const virus = [];
+const space = [];
+const ans = [];
+graph.forEach((elem, x) => elem.forEach((e, y) => {
+  if(e === 2) virus.push([x, y]);
+  if(e === 0) space.push([x, y]);
 }));
-//상하좌우
 const way = [[-1,0], [1,0], [0,-1], [0,1]];
-let max = 0;
 
-const bfs = () => {
-  let que = virus.slice();
-  
-  for(let i=0; i<N; i++){
-    for(let j=0; j<M; j++){
-      copyGraph[i][j] = graph[i][j];
-    }
-  }
-  
-  while(que.length){
-    const [x, y] = que.shift();
-    for(item of way){
-      const nx = x + item[0];
-      const ny = y + item[1];
 
-      if(nx < 0 || N <= nx || ny < 0 || M <= ny) continue;
-      if(copyGraph[nx][ny] === 1) continue;
-      if(copyGraph[nx][ny] === 0){
-        copyGraph[nx][ny] = 2;
-        que.push([nx, ny]);
+for(let i=0; i<space.length; i++){
+  for(let j=i+1; j<space.length; j++){
+    for(let k=j+1; k<space.length; k++){
+      let [x1, y1] = space[i];
+      let [x2, y2] = space[j];
+      let [x3, y3] = space[k];
+
+      let board = JSON.parse(JSON.stringify(graph));
+      board[x1][y1] = 1;
+      board[x2][y2] = 1;
+      board[x3][y3] = 1;
+
+      let que = JSON.parse(JSON.stringify(virus));
+      while(que.length){
+        const [x, y] = que.shift();
+        for(item of way){
+          const nx = x + item[0];
+          const ny = y + item[1];
+
+          if(nx<0 || N <= nx || ny<0 || M <= ny || board[nx][ny] === 1 || board[nx][ny] === 2) continue;
+          board[nx][ny] = 2;
+          que.push([nx,ny]);
+        }
       }
-    }
-  }
-
-  let zero=0;
-  copyGraph.forEach((elem) => elem.forEach(e => e === 0 ? zero++ : null));
-  return zero;
-}
-
-const wall = (x, y, cnt) => {
-  if(cnt == 3){
-    let ret = bfs();
-    max = Math.max(max, ret);
-  }
-  else{
-    for(let i=0; i<N; i++){
-      for(let j=0; j<M; j++){
-        if(graph[i][j] === 0){
-          graph[x][y] = 1;
-          wall(i,j, cnt+1);
-          graph[x][y] = 0;
-        }    
-      }
+      const safe = board.reduce((acc, curr) => acc + curr.filter(e => e === 0).length,0);
+      ans.push(safe);
     }
   }
 }
-for(let i=0; i<N; i++){
-  for(let j=0; j<M; j++){
-    if(graph[i][j] === 0){
-      graph[i][j] = 1;
-      wall(i,j, 0);
-      graph[i][j] = 0;
-    }
-  }
-}
-console.log(max);
+console.log(Math.max(...ans));
