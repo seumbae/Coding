67c84f80da9d7c1f71bb0c7606317401ec2b205e
seumbae <ohcm0013@gmail.com>
2023-02-25 04:04:38 +0900

    SELECT: GROUP BY HAVING, ORDER BY

---
+++ programmers/SQL 고득점 Kit/SELECT/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,9 @@
+SELECT USER_ID, PRODUCT_ID
+FROM ONLINE_SALE
+GROUP BY USER_ID, PRODUCT_ID
+HAVING 2 <= COUNT(*)
+ORDER BY USER_ID ASC, PRODUCT_ID DESC
+
+-- 단순히 HAVING없이 GROUP BY만 쓴다면 distinct를 사용하는 것과 별반 차이가 없다.
+-- 해당 칼럼의 중복을 제거하고 어떤 데이터가 있는지 보여주기 때문
+-- 하지만, HAVING 조건절과 같이 사용된다면 특정 데이터가 몇개 있는지 (count), 특정 데이터들의 합은 얼만지(sum), 특정 데이터들의 평균은 몇인지 등을 알 수 있다.
