9bfef32a669fe2d7ee3f58123bbfb277f6c8855c
seumbae <ohcm0013@gmail.com>
2023-02-26 22:49:42 +0900

    String, Date: LEFT

---
+++ programmers/SQL 고득점 Kit/String, Date/카테고리 별 상품 개수 구하기.sql
@@ -0,0 +1,8 @@
+SELECT LEFT(PRODUCT_CODE, 2) AS CATEGORY, COUNT(*) AS PRODUCTS
+FROM PRODUCT
+GROUP BY LEFT(PRODUCT_CODE, 2)
+ORDER BY CATEGORY
+
+-- LEFT는 주어진 문자열에서 왼쪽에서부터 몇자리의 문자를 가져옴
+-- 비슷한 함수로, RIGHT가 있으며 오른쪽에서부터 몇자리의 문자를 가져옴
+-- MID는 SUBSTR, SUBSTRING과 동일하며 (문자, 시작 위치, 가져올 갯수)이다.
