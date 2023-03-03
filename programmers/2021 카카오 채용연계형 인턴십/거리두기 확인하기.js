a36cb8c40acebd20e96dcb7f4b50cd80b740fd0c
seumbae <ohcm0013@gmail.com>
2023-03-03 23:35:14 +0900

    거리두기 확인하기: 2 conditions

--- programmers/2021 카카오 채용연계형 인턴십/거리두기 확인하기.js
+++ programmers/2021 카카오 채용연계형 인턴십/거리두기 확인하기.js
@@ -1,51 +1,37 @@
+// 1) 자기 원소가 P일 때, 상하좌우 원소에 P가 없다.
+// 2) 자기 원소가 O일 때, 상하좌우 원소에 P가 1개 이하이다.
+
 // |r1 - r2| + |c1 - c2|
 function solution(places) {
   var answer = [];
-  places.map((elem, idx) => {
-      const human = [];
-      elem.map((it, row) => {
-          for(let col = 0; col < 5; col++){
-              it[col] === 'P' ? human.push([row, col]) : null
-          }
-      })
-      
-      human.sort((a,b) => {
-          if(a[0] === b[0]) return a[1] - b[1]
-          return a[0] - b[0];
-      })
-      
-      for(let i=0; i<human.length-1; i++){
-          for(let j=i+1; j<human.length; j++){
-              const distance = Math.abs(human[i][0] - human[j][0]) + Math.abs(human[i][1] - human[j][1]);
+  const ways = [[-1, 0], [1, 0], [0, -1], [0, 1]]
   
-              if(distance === 2) {
-                  if(human[i][0] !== human[j][0] && human[i][1] !== human[j][1]){
-                      for(let k=0, r=1; k<2 && r>=0; k++, r--){
-                          const [_x, _y] = [human[i][k], human[j][r]];
-                          if(elem[idx][_x][_y] === 'O'){
-                              answer.push(0)
-                              return ;
-                          }
-                      }   
-                  }
-                  else if(human[i][0] === human[j][0]){
-                      const [_x, _y] = [human[i][0], human[i][1]+1];
-                      if(elem[idx][_x][_y] === 'O'){
-                              answer.push(0)
-                              return ;
-                      }
-                  }
-                  else if(human[i][1] === human[j][1]){
-                      const [_x, _y] = [human[i][0]+1, human[i][1]];
-                      if(elem[idx][_x][_y] === 'O'){
+  places.forEach((place) => {
+      for(let i=0; i<5; i++){
+          for(let j=0; j<5; j++){
+              if(place[i][j] === 'P'){
+                  for(let k=0; k<4; k++){
+                      const [nx, ny] = [i+ways[k][0], j+ways[k][1]];
+                      
+                      if(nx < 0 || 5 <= nx || ny < 0 || 5 <= ny) continue;
+                      if(place[nx][ny] === 'P'){
                           answer.push(0)
-                          return ;
+                          return;
                       }
                   }
               }
-              else if(distance < 2) {
-                  answer.push(0);
-                  return;
+              if(place[i][j] === 'O'){
+                  let pCnt = 0;
+                  for(let k=0; k<4; k++){
+                      const [nx, ny] = [i+ways[k][0], j+ways[k][1]];
+                      
+                      if(nx < 0 || 5 <= nx || ny < 0 || 5 <= ny) continue;
+                      if(place[nx][ny] === 'P') pCnt++;
+                  }
+                  if(2 <= pCnt){
+                      answer.push(0)
+                      return;
+                  }
               }
           }
       }
