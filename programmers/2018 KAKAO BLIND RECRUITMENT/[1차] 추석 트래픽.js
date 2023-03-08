44c59b3f340585795d6eabc2e06f0d7f6139be8e
seumbae <ohcm0013@gmail.com>
2023-03-08 17:36:04 +0900

    [1차] 추석 트래픽: Failed (only 1 case)

--- programmers/2018 KAKAO BLIND RECRUITMENT/[1차] 추석 트래픽.js
+++ programmers/2018 KAKAO BLIND RECRUITMENT/[1차] 추석 트래픽.js
@@ -1,33 +1,34 @@
-const strToTime = (line) => {
+const strToTime = (line, logs) => {
   const [date, time, completion] = line.split(' ');
   const [hour, minute, seconds] = time.split(':').map(e=>+e);
   
   const end = hour * 3600 + minute * 60 + seconds;
   const start = end - Number(completion.substring(0, completion.length-1)) + 0.001;
   
+  logs.push(start, end);
   return [start, end];
 }
 
 function solution(lines) {
   let ans = 0;
+  const logs = []
   
   lines.forEach((line, idx) => {
-      lines[idx] = strToTime(line);
+      lines[idx] = strToTime(line, logs);
   })
   
-  
-  lines.forEach((line, idx) => {
-      const [start, end] = line;
-      console.log('1',start, start+0.999)
+  logs.forEach((log, idx) => {
+      const start = log;
+      const end = start+0.999;
+      
       let cnt = 0;
       for(let i=0; i<lines.length; i++){
           const [from, to] = lines[i]; //ms단위
-          console.log('2',from, to)
           
-          if((to <= start && to < start+0.999) || 
-             (start <= from && from < start+0.999) ||
-             (from <= start && start+0.999 < to)
-           ) cnt++;
+          if((start <= to && to < end) || 
+             (start <= from && from < end) ||
+             (from <= start && end <= to)
+           ) {cnt++;}
       }
       ans = Math.max(ans, cnt);
   })
