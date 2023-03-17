353cdb3d869c5231330c4aa08bff5ee4c27fcfb7
seumbae <ohcm0013@gmail.com>
2023-03-17 21:35:46 +0900

    길 찾기 게임: tree, class

--- programmers/2019 KAKAO BLIND RECRUITMENT/길 찾기 게임.js
+++ programmers/2019 KAKAO BLIND RECRUITMENT/길 찾기 게임.js
@@ -1,3 +1,4 @@
+// case1
 const pre = [], post = [];
 let root;
 
@@ -53,6 +54,73 @@ function solution(nodeinfo) {
     })
     preorder(root);
     postorder(root);
+    
+    return [pre, post];
+}
+
+// case2
+const pre = [], post = [];
+
+class Node {
+    constructor(idx, x){
+        this.index = idx;
+        this.x = x;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Btree{
+    constructor(){
+        this.root = null;
+    }
+    
+    insert = (node) => {
+        if(!this.root){
+            this.root = node;
+            return this
+        }
+        
+        const set = (tree, node) => {
+            const next = tree.x < node.x ? 'right' : 'left';
+            if(tree[next] === null) tree[next] = node;
+            else set(tree[next], node);
+        }
+        set(this.root, node);
+    }
+    preorder = (tree) => {
+        let cur = tree === undefined ? this.root : tree;
+        if(cur){
+            pre.push(cur.index);
+            this.preorder(cur.left);
+            this.preorder(cur.right);
+        }
+    }
+
+    postorder = (tree) => {
+        let cur = tree === undefined ? this.root : tree;
+        if(cur){
+            this.postorder(cur.left);
+            this.postorder(cur.right);
+            post.push(cur.index);
+        }
+    } 
+}
+
+function solution(nodeinfo) {
+    nodeinfo = nodeinfo.map((elem,idx) => [elem[0], elem[1], idx+1]).sort((a, b) => {
+        if(a[1] === b[1]) return a[0] - b[0];
+        return b[1] - a[1];
+    })
+    
+    const tree = new Btree();
+    
+    nodeinfo.forEach(([x, y, idx]) => {
+        const node = new Node(idx, x);
+        tree.insert(node);
+    })
+    tree.preorder();
+    tree.postorder();
     
     return [pre, post];
 }
