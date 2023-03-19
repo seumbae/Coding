808de882decfc4428057ec0c2e3b168f032b1e21
seumbae <ohcm0013@gmail.com>
2023-03-19 21:55:40 +0900

    10807_개수 세기: list.count

--- baekjoon/python/1차원 배열/10807_개수 세기.py
+++ baekjoon/python/1차원 배열/10807_개수 세기.py
@@ -1,5 +1,12 @@
-from collections import Counter
-N = int(input())
-count = Counter(map(int, input().split()))
-toFind = int(input())
-print(count[toFind])
+# Case 1
+# from collections import Counter
+# N = int(input())
+# count = Counter(map(int, input().split()))
+# toFind = int(input())
+# print(count[toFind])
+
+from sys import stdin, stdout
+N = int(stdin.readline())
+numbers = list(map(int, stdin.readline().split()))
+toFind = int(stdin.readline())
+stdout.write(str(numbers.count(toFind)))
