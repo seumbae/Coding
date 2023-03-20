b46839fed716942bdaa5abf0a5ebdf98d43af392
seumbae <ohcm0013@gmail.com>
2023-03-20 17:04:33 +0900

    10988_팰린드롬인지 확인하기: reverserd

---
+++ baekjoon/python/심화 1/10988_팰린드롬인지 확인하기.py
@@ -0,0 +1,9 @@
+word = list(input())
+reversed_word = list(reversed(word))
+print(1 if word == reversed_word else 0)
+# flag = True
+# for i in range(len(word)//2):
+#     if(word[i] != word[-i-1]):
+#         flag = False
+#         break
+# print(1 if flag else 0)
