e38ffed6547faa35b676d212cb662a5082230679
seumbae <ohcm0013@gmail.com>
2023-04-20 18:04:03 +0900

    재구매가 일어난 상품과 회원 리스트 구하기: SELECT, GROUP BY HAVING, ORDER BY

--- programmers/SQL 고득점 Kit/SELECT/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ programmers/SQL 고득점 Kit/SELECT/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -2,7 +2,7 @@ SELECT USER_ID, PRODUCT_ID
 FROM ONLINE_SALE
 GROUP BY USER_ID, PRODUCT_ID
 HAVING 2 <= COUNT(*)
-ORDER BY USER_ID ASC, PRODUCT_ID DESC
+ORDER BY USER_ID, PRODUCT_ID DESC
 
 -- 단순히 HAVING없이 GROUP BY만 쓴다면 distinct를 사용하는 것과 별반 차이가 없다.
 -- 해당 칼럼의 중복을 제거하고 어떤 데이터가 있는지 보여주기 때문
