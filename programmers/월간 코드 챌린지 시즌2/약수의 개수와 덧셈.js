f466a4956799a2ae93a0d7bc332423369507b8d4
seumbae <ohcm0013@gmail.com>
2023-04-26 18:28:31 +0900

    약수의 개수와 덧셈: 제곱수의 약수 개수는 항상 홀수이다.

--- programmers/월간 코드 챌린지 시즌2/약수의 개수와 덧셈.js
+++ programmers/월간 코드 챌린지 시즌2/약수의 개수와 덧셈.js
@@ -1,3 +1,4 @@
+// Case 1:
 function solution(left, right) {
     const getDivisorCnt = num => {
         let cnt = 0;
@@ -12,6 +13,18 @@ function solution(left, right) {
         let tmp = getDivisorCnt(i)
         if(tmp % 2 === 0) ans += i
         else ans -= i
+    }
+    return ans
+}
+
+// Case 2: 재곱수의 약수의 개수는 홀수다.
+// ex. 9 => 1, 3, 9 : 1과 수, 그리고 제곱근
+function solution(left, right) {
+    let ans = 0
+    for(let i=left; i<=right; i++){
+        if(Number.isInteger(Math.sqrt(i)))
+            ans -= i
+        else ans += i
     }
     return ans
 }
