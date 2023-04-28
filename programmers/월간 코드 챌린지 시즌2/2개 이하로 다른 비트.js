8e38bdbbaafc4145f21c44eb9b1dd7c3fffb2c6d
seumbae <ohcm0013@gmail.com>
2023-04-28 15:21:26 +0900

    2개 이하로 다른 비트: cant use XOR

--- programmers/월간 코드 챌린지 시즌2/2개 이하로 다른 비트.js
+++ programmers/월간 코드 챌린지 시즌2/2개 이하로 다른 비트.js
@@ -1,15 +1,18 @@
 function solution(numbers) {
+    //XOR 연산: 대응되는 비트가 다른 경우 1을 반환
+    //number의 최대값이 10^15인데, 비트 연산자를 사용할 경우 32비트로 변환하고 상위 32비트는 버림으로써 다른 수가 되어버린다.
+    //따라서 비트연산자 사용 못함
     let ans = []
     numbers.forEach((number) => {
-        let num = number+1
-        while(true){
-            //XOR 연산: 대응되는 비트가 다른 경우 1을 반환
-            let cnt = (number^num).toString(2).split('').filter((elem) => elem === '1').length
-            if(cnt <= 2){
-                ans.push(num)
-                break
-            }
-            num++
+        if(number % 2 === 0){
+            ans.push(number+1)
+        }
+        //홀수인경우
+        else{
+            const str = ['0', ...number.toString(2).split('')]
+            const idx = str.lastIndexOf('0')
+            str[idx] = '1'; str[idx+1] = '0';
+            ans.push(parseInt(str.join(''), 2))
         }
     })
     return ans
