dffdee284195d26928e7b837fe464de46880b49c
seumbae <ohcm0013@gmail.com>
2023-05-01 15:18:53 +0900

    스타 수열: 모든 조합을 구하는 것이 아니다.

--- programmers/월간 코드 챌린지 시즌1/스타 수열.js
+++ programmers/월간 코드 챌린지 시즌1/스타 수열.js
@@ -1,49 +1,25 @@
-const getCombi = (arr, num) => {
-    const res = []
-    if(num === 1) return arr.map((el) => [el])
-    
-    arr.forEach((fixed, idx, origin) => {
-        const rest = origin.slice(idx+1)
-        const combi = getCombi(rest, num-1)
-        const attached = combi.map((el) => [fixed, ...el])
-        res.push(...attached)
-    })
-    return res
-}
-
 function solution(a) {
-    let len = a.length % 2 === 0 ? a.length : a.length-1
+    const count = a.reduce((acc, cur) =>{
+        acc[cur] = acc[cur]+1 || 1
+        return acc
+    },{})
+    const entries = Object.entries(count).sort((a, b) => b[1] - a[1])
     
     let ans = 0
-    for(let i = len; i>=2; i -= 2){
-        let combis = getCombi(a, i)
-        let f = true
-        combis.forEach((el) => {
-            const arr = []
-            for(let i=0; i<el.length-1; i += 2){
-                arr.push([el[i], el[i+1]])
-            }
-            let inter = 0
-            arr[0].forEach((el) => {
-                let flag = true
-                for(let i=1; i<arr.length; i++){
-                    if(!arr[i].includes(el)) flag = false
-                }
-                if(flag) inter++
-            })
-            if(inter === 0) return;
+    
+    entries.forEach((el) => {
+        const [key, cnt] = [+el[0], el[1]]
+        if(cnt < ans) return ;
+        
+        let res = 0
+        for(let i=0; i<a.length-1; i++){
+            if(a[i] !== key && a[i+1] !== key) continue
+            if(a[i] === a[i+1]) continue
             
-            let flag = true
-            arr.forEach((el) => {
-                if(el[0] === el[1]) flag = false
-            })
-            if(flag){
-                ans = el.length;
-                f = false
-                return;
-            }
-        })
-        if(!f) break
-    }
+            res += 2
+            i++
+        }
+        ans = Math.max(ans, res)
+    })
     return ans
 }
