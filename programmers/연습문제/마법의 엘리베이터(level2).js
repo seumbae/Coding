76673766740186ee9b47cfbcf123334f220d197d
seumbae <ohcm0013@gmail.com>
2023-05-15 19:59:41 +0900

    마법의 엘리베이터(level2): 경우의 수

--- programmers/연습문제/마법의 엘리베이터(level2).js
+++ programmers/연습문제/마법의 엘리베이터(level2).js
@@ -5,11 +5,11 @@ function solution(storey) {
         let remain = storey % 10;
         storey = Math.floor(storey/10)
         
-        if(remain < 5) ans += remain
-        else{
+        if(remain > 5 || (remain===5 && 5 <= storey%10)){
             storey += 1
-            ans += (10-remain)
+            remain = 10-remain
         }
+        ans += remain
     }
     return ans
 }
