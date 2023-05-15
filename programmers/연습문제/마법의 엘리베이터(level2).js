a1d7f6e5bfea0f32d56a9bdbf9c4e4028aaa6717
seumbae <ohcm0013@gmail.com>
2023-05-15 20:06:45 +0900

    마법의 엘리베이터(level2): dfs

--- programmers/연습문제/마법의 엘리베이터(level2).js
+++ programmers/연습문제/마법의 엘리베이터(level2).js
@@ -1,15 +1,32 @@
 function solution(storey) {
-    let ans = 0;
-    
-    while(storey){
-        let remain = storey % 10;
-        storey = Math.floor(storey/10)
+    //     let ans = 0;
         
-        if(remain > 5 || (remain===5 && 5 <= storey%10)){
-            storey += 1
-            remain = 10-remain
+    //     while(storey){
+    //         let remain = storey % 10;
+    //         storey = Math.floor(storey/10)
+            
+    //         if(remain > 5 || (remain===5 && 5 <= storey%10)){
+    //             storey += 1
+    //             remain = 10-remain
+    //         }
+    //         ans += remain
+    //     }
+    //     return ans
+        
+        let ans = Number.MAX_SAFE_INTEGER;
+        
+        const dfs = (cmp, cnt) => {
+            if(ans < cnt) return;
+            if(cmp === 0){
+                ans = cnt
+            } 
+            else{
+                let remain = cmp % 10;
+                
+                dfs(Math.floor(cmp/10), cnt + remain);
+                dfs(Math.floor(cmp/10)+1, cnt + 10 - remain)
+            }
         }
-        ans += remain
+        dfs(storey, 0)
+        return ans
     }
-    return ans
-}
