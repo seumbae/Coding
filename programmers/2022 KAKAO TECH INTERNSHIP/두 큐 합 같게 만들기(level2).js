863aead4a32bbc94fd033322d62288709a9a2b4b
seumbae <ohcm0013@gmail.com>
2023-05-23 17:41:13 +0900

    두 큐 합 같게 만들기(level2): maxCount

--- programmers/2022 KAKAO TECH INTERNSHIP/두 큐 합 같게 만들기(level2).js
+++ programmers/2022 KAKAO TECH INTERNSHIP/두 큐 합 같게 만들기(level2).js
@@ -3,34 +3,26 @@ function solution(queue1, queue2) {
     let que2 = queue2.reduce((acc, cur) => acc+cur, 0)
     const total = que1 + que2;
     
-    if(queue1.some(el => el > Math.floor(total/2)) || queue2.some(el => el > Math.floor(total/2)))
+    if(queue1.some(el => el > Math.floor(total/2)) || queue2.some(el => el > Math.floor(total/2)) || total%2 !== 0)
         return -1;
     
     let cnt = 0
-    while(que1 !== que2){
-        if(queue1[0] === Math.floor(total/2)){
-            que1 -= queue1[0]
-            que2 += queue1[0]
-            queue2.push(queue1.shift())
-            
+    let val = que1 - que2;
+    let q1 = 0, q2 = 0;
+    const maxCnt = queue1.length*3
+
+    while(cnt < maxCnt && val !== 0){
+        if(val < 0){ //que1 < que2
+            const v = queue2[q2++]
+            queue1.push(v)
+            val += (v*2)
         }
-        else if(queue2[0] === Math.floor(total/2)){
-            que2 -= queue2[0]
-            que1 += queue2[0]
-            queue1.push(queue2.shift())
-        }
-        else if(que1 < que2){
-            que2 -= queue2[0]
-            que1 += queue2[0]
-            queue1.push(queue2.shift())
-        }
-        else if(que2 < que1){
-            que1 -= queue1[0]
-            que2 += queue1[0]
-            queue2.push(queue1.shift())
+        else{
+            const v = queue1[q1++]
+            queue2.push(v)
+            val -= (v*2)
         }
         cnt++
     }
-    return cnt
-    
+    return val === 0 ? cnt : -1
 }
