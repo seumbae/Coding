9aab5f9df456756fbaa020f11c2689dc8e32a033
seumbae <ohcm0013@gmail.com>
2023-05-31 19:28:57 +0900

    전력망을 둘로 나누기(level2): bfs

--- programmers/완전탐색/전력망을 둘로 나누기(level2).js
+++ programmers/완전탐색/전력망을 둘로 나누기(level2).js
@@ -8,47 +8,25 @@ function solution(n, wires) {
         graph[to].push(from)
     })
     
-    const bfs = (idx, wire) =>{
-        const visited = Array(n).fill(false);
-        visited[idx-1] = true;
-        let cnt = 0
+    for(let wire of wires){
+        const [start, end] = wire
+        const visited = [...Array(n+1)].fill(false);
+        const que = [...graph[start]]
+
+        visited[start] = true;
+        visited[end] = true;
         
-        const que = [idx]
-        
+        let connection = 1
         while(que.length){
-            const node = que.shift();
-            
-            wire[node].forEach((to) => {
-                if(!visited[to-1]) {
-                    que.push(to)
-                    visited[to-1] = true
-                    cnt++
-                }
-            })
-        }
-        return cnt+1
-    }
-    const dfs = (wire, depth, node) => {
-        if(depth === 1){
-            const connection = bfs(node, wire)
-            const rest = Math.abs(n - connection)
-            
-            ans = Math.min(ans, Math.abs(rest - connection))
-        }
-        for(let i=1; i<=n; i++){
-            for(let j=0; j<wire[i].length; j++){
-                const toDelete = wire[i][0];
-                wire[i].splice(0, 1);
-                const idx = wire[toDelete].indexOf(i);
-                wire[toDelete].splice(idx, 1)
-                
-                dfs(wire.slice(), depth+1, i)
-                
-                wire[i].push(toDelete);
-                wire[toDelete].push(i);
+            const next = que.shift();
+            if(!visited[next]){
+                visited[next] = true;
+                connection++;
+                que.push(...graph[next])
             }
         }
+        ans = Math.min(Math.abs(n- 2*connection), ans);
     }
-    dfs(graph.slice(), 0, 1)
+    
     return ans;
 }
